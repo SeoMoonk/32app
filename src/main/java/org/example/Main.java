4ce0e026a6c5053a87fde526b5e5c3194b256ced
SeoMoonk <sunghoon1216@naver.com>
2023-03-02 09:38:31 +0900

    work-5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +14,11 @@ public class Main {
         String author = "";
 
         int count = 0;
+
+        List<String> saying_arr = new ArrayList<>();
+        List<String> author_arr = new ArrayList<>();
+
+
 
 
         System.out.println("== 명언 앱 ==");
@@ -30,17 +37,28 @@ public class Main {
             {
                 System.out.print("명언 : ");
                 saying = sc.nextLine();
+                saying_arr.add(count, saying);
 
                 System.out.print("작가 : ");
                 author = sc.nextLine();
+                author_arr.add(count, author);
 
                 count ++;
 
                 System.out.printf("%d 번 명언이 등록되었습니다. \n", count);
 
-
             }
+            else if(input.equals("목록"))
+            {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-----------------------");
 
+                for(int i = count-1; i >= 0; i--)
+                {
+                    System.out.printf("%d / %s / %s \n", i+1, author_arr.get(i), saying_arr.get(i));
+
+                }
+            }
 
         }
 
