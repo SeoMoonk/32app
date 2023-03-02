d1a3155c30c93802655504c5b8c3338fe2025a68
SeoMoonk <sunghoon1216@naver.com>
2023-03-02 10:38:11 +0900

    work-8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,16 +9,17 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String input = "";
-        String saying = "";
-        String author = "";
+        String input;
+        String saying;
+        String author;
+        String input_saying;
+        String input_author;
 
+        int del_num = 0;
+        int crt_num = 0;
         int count = 0;
 
-        List<String> saying_arr = new ArrayList<>();
-        List<String> author_arr = new ArrayList<>();
-
-
+        List<Wise_saying> wise_saying = new ArrayList<>();
 
 
         System.out.println("== 명언 앱 ==");
@@ -37,13 +38,13 @@ public class Main {
             {
                 System.out.print("명언 : ");
                 saying = sc.nextLine();
-                saying_arr.add(count, saying);
 
                 System.out.print("작가 : ");
                 author = sc.nextLine();
-                author_arr.add(count, author);
 
                 count ++;
+
+                wise_saying.add(new Wise_saying(count, saying, author));
 
                 System.out.printf("%d 번 명언이 등록되었습니다. \n", count);
 
@@ -55,14 +56,92 @@ public class Main {
 
                 for(int i = count-1; i >= 0; i--)
                 {
-                    System.out.printf("%d / %s / %s \n", i+1, author_arr.get(i), saying_arr.get(i));
+                    System.out.printf("%d / %s / %s \n", wise_saying.get(i).number, wise_saying.get(i).author, wise_saying.get(i).saying);
 
                 }
+            }
+            else if(input.equals("삭제"))
+            {
+                System.out.print("?id=");
+
+                input = sc.nextLine();
+                del_num = Integer.parseInt(input);
+
+                //유효한 삭제인지 검사
+                for(int i=0; i<=wise_saying.size(); i++)
+                {
+                    if(del_num == wise_saying.get(i).number)
+                    {
+                        del_num--;
+
+                        wise_saying.remove(del_num);
+                        count--;
+
+                        System.out.printf("%d 번 명언이 삭제되었습니다.\n",del_num+1);
+                        break;
+                    }
+
+                    if(i == wise_saying.size()-1)
+                    {
+                        System.out.printf("%d 번 명언은 존재하지 않습니다. \n", del_num);
+                        break;
+                    }
+
+                }
+            }
+            else if(input.equals("수정"))
+            {
+                System.out.print("?id=");
+                input = sc.nextLine();
+                crt_num = Integer.parseInt(input);
+
+                for(int i=0; i<=wise_saying.size(); i++)
+                {
+                    //수정할 명언을 찾은 경우
+                    if(crt_num == wise_saying.get(i).number)
+                    {
+
+                        System.out.printf("명언(기존) : %s \n", wise_saying.get(crt_num-1).saying);
+                        System.out.print("명언 : ");
+                        input_saying = sc.nextLine();
+
+                        System.out.printf("작가(기존) : %s \n", wise_saying.get(crt_num-1).author);
+                        System.out.print("작가 : ");
+                        input_author = sc.nextLine();
+
+                        wise_saying.set(crt_num-1, new Wise_saying(crt_num, input_saying, input_author));
+
+                        break;
+                    }
+
+                    //수정할 명언을 못찾은 경우
+                    if(i == wise_saying.size()-1)
+                    {
+
+                    }
+
+                }
+
+
+
             }
 
         }
 
     }
 
+}
+
+class Wise_saying{
+
+    int number;
+    String saying;
+    String author;
+
+    public Wise_saying(int number, String saying, String author) {
+        this.number = number;
+        this.saying = saying;
+        this.author = author;
+    }
 
 }
