ef640bae0e5c792c3ae70cdcb89672315d454d8b
SeoMoonk <sunghoon1216@naver.com>
2023-03-02 09:24:01 +0900

    work-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,11 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String input;
+        String input = "";
+        String saying = "";
+        String author = "";
+
+        int count = 0;
 
 
         System.out.println("== 명언 앱 ==");
@@ -16,10 +20,25 @@ public class Main {
         {
             System.out.print("명령) ");
             input = sc.nextLine();
+
             if(input.equals("종료"))
             {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
+            }
+            else if(input.equals("등록"))
+            {
+                System.out.print("명언 : ");
+                saying = sc.nextLine();
+
+                System.out.print("작가 : ");
+                author = sc.nextLine();
+
+                count ++;
+
+                System.out.printf("%d 번 명언이 등록되었습니다. \n", count);
+
+
             }
 
 
